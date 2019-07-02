1a2f96c50541df801c6e425ac236d75e959e94b5
aribot <alexisribot10@gmail.com>
2019-07-02 23:42:46 +0200

    Add loading, search and poster image, use TMDB

---
+++ API/TMDBApi.js
@@ -0,0 +1,12 @@
+const API_TOKEN = "ad8fc88360e3f0fb37797f4b04d0a33e";
+
+export function getFilmsFromApiWithSearchedText(text) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text;
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.error(error))
+}
+
+export function getImageFromApi(name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
+}
--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import { StyleSheet, View, Text, Image } from 'react-native'
+import { getImageFromApi } from "../API/TMDBApi";
 
 class FilmItem extends React.Component {
   render() {
+    const film = this.props.film
     return (
       <View style={styles.main_container}>
         <Image
           style={styles.image}
-          source={{uri: "image"}}
+          source={{uri: getImageFromApi(film.poster_path)}}
         />
         <View style={styles.content_container}>
           <View style={styles.header_container}>
-            <Text style={styles.title_text}>Titre du film</Text>
-            <Text style={styles.vote_text}>Vote</Text>
+            <Text style={styles.title_text}>{film.title}</Text>
+            <Text style={styles.vote_text}>{film.vote_average}</Text>
           </View>
           <View style={styles.description_container}>
-            <Text style={styles.description_text} numberOfLines={6}>Description</Text>
+            <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
           </View>
           <View style={styles.date_container}>
-            <Text style={styles.date_text}>Sorti le 00/00/0000</Text>
+            <Text style={styles.date_text}>Sorti le {film.release_date}</Text>
           </View>
         </View>
       </View>
@@ -34,8 +36,7 @@ const styles = StyleSheet.create({
   image: {
     width: 120,
     height: 180,
-    margin: 5,
-    backgroundColor: 'gray'
+    margin: 5
   },
   content_container: {
     flex: 1,
