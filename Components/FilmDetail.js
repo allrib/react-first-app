7da330b072f6858c8849ae8b4377ad3d133c0a99
aribot <alexisribot10@gmail.com>
2019-07-03 17:48:26 +0200

    WIP : detail film

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,12 +1,51 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, ActivityIndicator, Text, ScrollView } from 'react-native'
+import { getFilmDetailFromApi } from "../API/TMDBApi";
 
 class FilmDetail extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            film: undefined, // Pour l'instant on n'a pas les infos du film, on initialise donc le film à undefined.
+            isLoading: true // A l'ouverture de la vue, on affiche le chargement, le temps de récupérer le détail du film
+        }
+    }
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+            // Si isLoading vaut true, on affiche le chargement à l'écran
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large' />
+                </View>
+            )
+        }
+    }
+
+    componentDidMount() {
+        getFilmDetailFromApi(this.props.navigation.state.params.idFilm).then(data => {
+            this.setState({
+                film: data,
+                isLoading: false
+            })
+        })
+    }
+
+    _displayFilm() {
+        if (this.state.film != undefined) {
+            return (
+                <ScrollView style={styles.scrollview_container}>
+                    <Text>{this.state.film.title}</Text>
+                </ScrollView>
+            )
+        }
+    }
+
     render() {
-        console.log(this.props.navigation);
         return (
-            <View styles={styles.main_container}>
-                <Text>Détail du film {this.props.navigation.state.params.idFilm}</Text>
+            <View style={styles.main_container}>
+                {this._displayLoading()}
+                {this._displayFilm()}
             </View>
         )
     }
@@ -14,7 +53,19 @@ class FilmDetail extends React.Component {
 
 const styles = StyleSheet.create({
     main_container: {
-        flex: 1,
+        flex: 1
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    scrollview_container: {
+        flex: 1
     }
 });
 
