8a971e73573a312edf4a446e564dbe3d58f762c1
aribot <alexisribot10@gmail.com>
2019-07-04 17:47:30 +0200

    Film detail

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,24 +1,15 @@
 import React from 'react'
-import { StyleSheet, View, ActivityIndicator, Text, ScrollView } from 'react-native'
-import { getFilmDetailFromApi } from "../API/TMDBApi";
+import { StyleSheet, View, Text, ActivityIndicator, ScrollView, Image } from 'react-native'
+import { getFilmDetailFromApi, getImageFromApi } from '../API/TMDBApi'
+import moment from 'moment'
+import numeral from 'numeral'
 
 class FilmDetail extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            film: undefined, // Pour l'instant on n'a pas les infos du film, on initialise donc le film à undefined.
-            isLoading: true // A l'ouverture de la vue, on affiche le chargement, le temps de récupérer le détail du film
-        }
-    }
-
-    _displayLoading() {
-        if (this.state.isLoading) {
-            // Si isLoading vaut true, on affiche le chargement à l'écran
-            return (
-                <View style={styles.loading_container}>
-                    <ActivityIndicator size='large' />
-                </View>
-            )
+            film: undefined,
+            isLoading: true
         }
     }
 
@@ -31,11 +22,39 @@ class FilmDetail extends React.Component {
         })
     }
 
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large' />
+                </View>
+            )
+        }
+    }
+
     _displayFilm() {
-        if (this.state.film != undefined) {
+        const { film } = this.state
+        if (film != undefined) {
             return (
                 <ScrollView style={styles.scrollview_container}>
-                    <Text>{this.state.film.title}</Text>
+                    <Image
+                        style={styles.image}
+                        source={{uri: getImageFromApi(film.backdrop_path)}}
+                    />
+                    <Text style={styles.title_text}>{film.title}</Text>
+                    <Text style={styles.description_text}>{film.overview}</Text>
+                    <Text style={styles.default_text}>Sorti le {moment(new Date(film.release_date)).format('DD/MM/YYYY')}</Text>
+                    <Text style={styles.default_text}>Note : {film.vote_average} / 10</Text>
+                    <Text style={styles.default_text}>Nombre de votes : {film.vote_count}</Text>
+                    <Text style={styles.default_text}>Budget : {numeral(film.budget).format('0,0[.]00 $')}</Text>
+                    <Text style={styles.default_text}>Genre(s) : {film.genres.map(function(genre){
+                        return genre.name;
+                    }).join(" / ")}
+                    </Text>
+                    <Text style={styles.default_text}>Companie(s) : {film.production_companies.map(function(company){
+                        return company.name;
+                    }).join(" / ")}
+                    </Text>
                 </ScrollView>
             )
         }
@@ -66,7 +85,34 @@ const styles = StyleSheet.create({
     },
     scrollview_container: {
         flex: 1
+    },
+    image: {
+        height: 169,
+        margin: 5
+    },
+    title_text: {
+        fontWeight: 'bold',
+        fontSize: 35,
+        flex: 1,
+        flexWrap: 'wrap',
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
+        marginBottom: 10,
+        color: '#000000',
+        textAlign: 'center'
+    },
+    description_text: {
+        fontStyle: 'italic',
+        color: '#666666',
+        margin: 5,
+        marginBottom: 15
+    },
+    default_text: {
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 5,
     }
-});
+})
 
 export default FilmDetail
